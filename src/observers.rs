8f207a2a93ae6c8f4153471e8453afc31748e0c8
Florian Schnell <florian.schnell@netlight.com>
2020-07-14 17:22:27 +0200

    remove firebase

--- src/observers.rs
+++ src/observers.rs
@@ -1,7 +1,6 @@
 mod csv;
 mod debug;
 mod dynamodb;
-mod firebase;
 mod mail;
 mod observer;
 mod telegram;
@@ -9,7 +8,6 @@ mod telegram;
 pub use crate::observers::csv::CSV;
 pub use crate::observers::debug::DebugObserver;
 pub use crate::observers::dynamodb::DynamoDbObserver;
-pub use crate::observers::firebase::Firebase;
 pub use crate::observers::mail::Mail;
 pub use crate::observers::observer::{Observer, ObserverError};
 pub use crate::observers::telegram::Telegram;
@@ -19,7 +17,6 @@ use crate::ApplicationConfig;
 pub fn get_observers(app_config: &ApplicationConfig) -> Vec<Box<dyn Observer>> {
   let observers: Vec<Box<dyn Observer>> = vec![
     Box::new(Telegram {}),
-    Box::new(Firebase::new(app_config)),
     Box::new(Mail {}),
     Box::new(CSV {}),
     Box::new(DynamoDbObserver::new(app_config)),
